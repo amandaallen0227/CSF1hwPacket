e9efb17cb40d5b33654ef3898913b8c1d5bc96ed
Amanda Allen <amandaallen0227@gmail.com>
2021-07-24 23:21:39 -0500

    FruitLoop warm up part 1 complete.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+
+            // Declare and Initialize array of 6 fruit names (as text)
+
+            string[] fruits = { "Pinapple", "Strawberry", "Watermelon", "Orange", "Apple", "Rasberry" };
+
+            // Set up a FOR loop and iterate through them
+            // Display each value in the console
+
+            foreach (string fruit in fruits)
+            {
+
+                Console.WriteLine(fruit);
+            }
+            //int fruitsInt = Convert.ToInt32(fruits);
+
+            //for(int i = 0; i <= fruits.Length; i++)
+            //{
+                //Console.WriteLine("Here is a fruit, ", fruits);
+           // }
+        }// END MAIN
+
+    } // END CLASS
+
+} // END NAMESPACE
